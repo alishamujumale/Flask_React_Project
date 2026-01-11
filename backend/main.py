e19c623d1f6a2dcdf4853e9773ea01089eb6fe59
alishamujumale <alishamujumale@gmail.com>
2026-01-11 15:10:39 +0530

    run.py

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,36 @@
-from flask import Flask, request,jsonify
-from flask_restx import Api, Resource, fields
+from flask import Flask
+from flask_restx import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash,check_password_hash
-
 from config import DevConfig
 from models import Recipe, User
 from exts import db
-from flask_jwt_extended import JWTManager,create_access_token,create_refresh_token,jwt_required
-
-app = Flask(__name__)
-CORS(app)
-app.config.from_object(DevConfig)
-
-db.init_app(app)
-migrate = Migrate(app, db)
-JWTManager(app)
-
-api = Api(app, doc='/docs')
-
-# Namespaces
-auth_ns = api.namespace('auth', description='Authentication APIs')
-recipe_ns = api.namespace('recipes', description='Recipe APIs')
+from flask_jwt_extended import JWTManager
+from recipes import recipe_ns
+from auth import auth_ns
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def create_app(config=DevConfig):   
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config.from_object(config)  
+
+    db.init_app(app)
+    Migrate(app, db)                
+    JWTManager(app)
+
+    api = Api(app, doc='/docs')
+
+    api.add_namespace(recipe_ns)
+    api.add_namespace(auth_ns)
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            "db": db,
+            "Recipe": Recipe,
+            "User": User              
+        }
+
+    return app
---
+++ backend/run.py
@@ -0,0 +1,6 @@
+from main import create_app
+from config import DevConfig,TestConfig
+
+if __name__ == '__main__':
+    app=create_app(TestConfig)
+    app.run()
